fd5102927e73d484b4fb41908a6b1737f19cd017
_siddheshkavitkar <siddheshkavitkar07@gmail.com>
2026-04-05 18:50:15 +0530

    Create led_Patterns.h for LED patterns
    Add header file for LED patterns functionality

---
+++ Drivers/Usart/LED/led_Patterns.h
@@ -0,0 +1,15 @@
+/*
+ * led_Patterns.h
+ *
+ *  Created on: Apr 5, 2026
+ *      Author: siddh
+ */
+
+#ifndef INC_LED_PATTERNS_H_
+#define INC_LED_PATTERNS_H_
+
+#include"stm32f4xx.h"
+#include "main.h"
+#include "stdint.h"
+
+#endif /* INC_LED_PATTERNS_H_ */
